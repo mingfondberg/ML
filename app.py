534f7256b5311629907b91e704d1846349eed77a
mingfondberg <ming.fondberg@gmail.com>
2024-03-25 04:16:45 +0100

    Lägger till filer

---
+++ app.py
@@ -0,0 +1,10 @@
+
+import cv2
+
+file_1 = r""C:\Users\Admin\Desktop\‫ML\WIN_20240321_11_28_14_Pro.jpg"
+test_image_1 = cv2.imread(file_1, cv2.IMREAD_GRAYSCALE)
+
+file_2 = r"C:\Users\Admin\Desktop\‫ML\WIN_20240321_11_30_00_Pro.jpg"
+test_image_2 = cv2.imread(file_2, cv2.IMREAD_GRAYSCALE)
+
+file_3 = r"C:\Users\Admin\Desktop\‫ML\WIN_20240321_11_29_48_Pro.jpg"
